cb0597df79a011957f2417bda27cc1ec17e47641
Curtis Haynes <cchaynes1122@gmail.com>
2022-01-08 18:15:34 -0800

    Achieve parity with previous project

---
+++ src/components/Document.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <textarea v-model="textInput" id="document" name="document"> </textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Document",
+  props: {},
+  data() {
+    return {
+      textInput: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#document {
+  background-color: white;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  padding: 20px;
+  font-size: 12px;
+}
+textarea {
+  /* Disable default styling */
+  border: none;
+  overflow: auto;
+  outline: none;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+  resize: none;
+  /* Set height and width */
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/DocumentView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="toolbar">
+      <v-btn icon color="pink">
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+    </div>
+    <div class="document-container">
+      <Document class="document" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Document from "../components/Document.vue";
+
+export default {
+  name: "DocumentView",
+  components: {
+    Document,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  mounted() {},
+};
+</script>
+
+<style scoped>
+.toolbar {
+  background-color: #f5f5f5;
+  height: 50px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+.document-container {
+  display: flex;
+  justify-content: center;
+  background-color: #fafafa;
+  width: 100%;
+  min-height: 100vh;
+  padding-top: 20px;
+}
+.document {
+  width: 80vw;
+  min-height: 100vh;
+}
+</style>
